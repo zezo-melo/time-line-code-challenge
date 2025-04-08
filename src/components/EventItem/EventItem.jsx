71a2fdc48aa15f2e2485506174689a80d30f9299
zezo-melo <robertomello404@gmail.com>
2025-04-08 17:32:19 -0300

    Changes: some design changes, added new colors to the events

--- src/components/EventItem/EventItem.jsx
+++ src/components/EventItem/EventItem.jsx
@@ -5,15 +5,18 @@ import "./EventItem.css"
 export default function EventItem({ item }) {
   const left = getLeftOffset(item.start);
   const width = getWidth(item.start, item.end);
+  const colors = ["#4a90e2", "#50e3c2", "#f5a623", "#bd10e0", "#7ed321"];
+  const backgroundColor = colors[item.id % colors.length];
 
   return (
     <div
       className="event"
       style={{
-        position: "absolute",
-        top: 0,
         left: `${left}px`,
         width: `${width}px`,
+        backgroundColor,
+        position: "absolute",
+        top: 0,
       }}
     >
       {item.name}
